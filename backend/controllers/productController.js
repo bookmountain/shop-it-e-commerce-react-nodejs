78f9d8794a2203a57125fb36d45eb1f7e0ec2594
book33 <bookmountain0222@gmail.com>
2021-12-16 16:14:13 +0800

    Feature: add product filter

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -22,15 +22,19 @@ exports.getProducts = catchAsyncError(async (req, res, next) => {
 
   const apiFeatures = new APIFeatures(Product.find(), req.query)
     .search()
-    .filter()
-    .pagination(resPerPage);
+    .filter();
 
-  const products = await apiFeatures.query;
+  let products = await apiFeatures.query;
+  let filteredProductsCount = products.length;
+
+  apiFeatures.pagination();
 
   res.status(200).json({
     success: true,
     count: products.length,
     productsCount,
+    resPerPage,
+    filteredProductsCount,
     products,
   });
 });
